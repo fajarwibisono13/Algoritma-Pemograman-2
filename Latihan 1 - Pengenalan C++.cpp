85adce83384df4157cf93168545ece34c2e9644b
Muhammad Fajar Wibisono <muhammadfajar1345@gmail.com>
2017-03-13 11:50:35 +0700

    First Project - Hello World

---
+++ Latihan 1 - Pengenalan C++.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	cout << "Hello World" << endl ;
+	cout << "I Love Living In This World" << endl;
+	cout << "Muhammad Faja Wibisono" << endl;
+	cout << "TIF 16 B Malam" << endl;
+	return 0;
+	
+}
