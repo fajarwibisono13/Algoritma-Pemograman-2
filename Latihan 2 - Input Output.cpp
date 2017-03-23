84b6791312d288b16a77a73aa270ec3ccd5f9974
Muhammad Fajar Wibisono <muhammadfajar1345@gmail.com>
2017-03-23 13:57:06 +0700

    Add files via upload

---
+++ Latihan 2 - Input Output.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+main (){
+	char nama[12],kelas[12];
+	int nim;
+	
+	
+	cout << "masukan nama  : " ;
+	cin >> nama ; 
+	cout << "masukan kelas  : " ;
+	cin >> kelas; 
+	cout << "masukan NIM   : " ;
+	cin >> nim; 
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	
+	cout <<"========================== \n";
+	cout << "nama kamu adalah : "; cout << nama; cout <<"\n" ;
+	cout << "kelas  : "; cout << kelas; cout <<"\n";
+	cout << "nomor NIM kamu : "; cout << nim; cout << "\n" ;
+	cout <<"========================== \n";
+	
+	return 0;
+	
+	
+}
+	
