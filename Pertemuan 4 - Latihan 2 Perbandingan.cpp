6166168f19adbcfad56e0afb6c39c9da98c0abdb
Muhammad Fajar Wibisono <muhammadfajar1345@gmail.com>
2017-03-31 20:15:17 +0700

    Pertemuan 4 [31 Maret 2017]

---
+++ Pertemuan 4 - Latihan 2 Perbandingan.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int perbandingan (int a, int b){
+
+
+	if (a > b){
+		return a;
+	}
+	else {
+		
+		return b;
+	}
+	
+}
+
+	
+int main (){
+	
+	cout << "Hasilnya adalah : " << perbandingan (99,20);
+	return 0;
+
+}
